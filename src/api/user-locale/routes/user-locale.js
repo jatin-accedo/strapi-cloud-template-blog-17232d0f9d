1e3dc65fb142b5f1f6c54ae098554ae7e3582433
Craig Martin <craig.martin@accedo.tv>
2024-07-03 14:22:52 +1000

    Get Elevate objects

---
+++ src/api/user-locale/routes/user-locale.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-locale router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-locale.user-locale');
